59089a417ed52baf451feac506c6b1ec4d6b7bed
Partizanzero <ElenaD81@list.ru>
2024-06-16 23:33:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,8 @@
 const loadBtn = document.querySelector(".main__load-btn"); //кнопка Загрузить картинки
 const clearBtn = document.querySelector(".main__clear-btn"); //кнопка Удалить картинки
 const postsContainer = document.querySelector(".main__posts-container"); //див, в который будем загружать все картинки
-const loader = document.querySelector(".main__posts-container-loader"); //див с лоадером
-
-loader.style.display = "none";
+const img = document.querySelector(".main__posts-container-img img"); //картинка
+const loader = document.querySelector(".main__posts-container-loader"); //лоадер
 
 //2. Делаем запрос на сервер через fetch()
 const fetchData = () => {
@@ -14,27 +13,22 @@ const fetchData = () => {
 
     //если ответ от сервера пришел - обрабатываем его
     .then((data) => {
-      //деалем лоадер видимым до того, как начнется отрисовка картинки в диве
-      loader.style.display = "block";
+      //проходим по массиву пришедших данных и для каждого элемента массива создаем див
+      data.forEach((post) => {
+        //создаем див, куда будем выводить конкретный пост
+        const postElement = document.createElement("div");
+        //задаем созданному диву класс main__posts-container-img
+        postElement.classList.add("main__posts-container-img");
 
-      //интервал 5 сек до начала отрисовки картинок, чтобы прокрутился лоадер
-      setTimeout(() => {
-        //проходим по массиву пришедших данных и для каждого элемента массива создаем див
-        data.forEach((post) => {
-          //создаем див, куда будем выводить конкретный пост
-          const postElement = document.createElement("div");
-          //задаем созданному диву класс main__posts-container-img
-          postElement.classList.add("main__posts-container-img");
-          //заменяем содержимое созданного дива на пост
-          postElement.innerHTML = `
-<a href="${post.url}" target="_blank"><img src="${post.url}" alt="Картинка с котом ${post.id}" /></a>
+        //заменяем содержимое созданного дива на пост
+        postElement.innerHTML = `
+<a href="${post.url}" target="_blank"><img src="${post.url}" alt="Картинка с котом ${post.id}" style="display:none;" /></a>
+<div class="main__posts-container-loader"></div>
 `;
-          //добавляем блок с текущим постом в конец дива со всеми постами
-          postsContainer.appendChild(postElement);
-          //скрываем лоадер после загрузки картинок
-          loader.style.display = "none";
-        });
-      }, 5000);
+
+        //добавляем блок с текущим постом в конец дива со всеми постами
+        postsContainer.appendChild(postElement);
+      });
     })
 
     .catch((error) => {
@@ -50,3 +44,23 @@ const cleanData = () => {
 //4. Вешаем Обработчики Событий на кнопки
 loadBtn.addEventListener("click", fetchData);
 clearBtn.addEventListener("click", cleanData);
+
+//если картинка загрузилась - прячем лоадер и показываем картинку
+img.addEventListener("load", function () {
+  loader.style.display = "none";
+  img.style.display = "block";
+});
+
+img.addEventListener('error', function() {
+  loader.style.display = 'none';
+  img.style.display = 'none';
+  console.error('Ошибка загрузки изображения');
+});
+/*
+
+не получается вставить данный код так, чтобы он менял стили у картинки и лоадера
+        img.addEventListener("load", function () {
+          loader.style.display = "none";
+          img.style.display = "block";
+        });
+*/
