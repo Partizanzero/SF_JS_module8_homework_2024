de02886c304243522d4927e6b0aa36604af968a9
Partizanzero <ElenaD81@list.ru>
2024-06-17 22:49:34 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,65 @@
 //1. Получаем нужные элементы
-
+const URL = "https://api.thecatapi.com/v1/images/search?limit=10"; //адрес на сервере, к которому обращаемся
 const loadBtn = document.querySelector(".main__load-btn"); //кнопка Загрузить картинки
 const clearBtn = document.querySelector(".main__clear-btn"); //кнопка Удалить картинки
 const postsContainer = document.querySelector(".main__posts-container"); //див, в который будем загружать все картинки
-const img = document.querySelector(".main__posts-container-img img"); //картинка
+const img = document.querySelector(".main__posts-container-img img"); //картинка из json
 const loader = document.querySelector(".main__posts-container-loader"); //лоадер
 
 //2. Делаем запрос на сервер через fetch()
-const fetchData = () => {
-  fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-    .then((response) => response.json()) //говорим серверу, чтобы вернул ответ в json
-
+const fetchData = async () => {
+  try {
+    let data = await fetch(URL); //отправляем запрос на сервер
+    let response = await data.json(); //говорим серверу, чтобы вернул ответ в json
     //если ответ от сервера пришел - обрабатываем его
-    .then((data) => {
-      //проходим по массиву пришедших данных и для каждого элемента массива создаем див
-      data.forEach((post) => {
-        //создаем див, куда будем выводить конкретный пост
-        const postElement = document.createElement("div");
-        //задаем созданному диву класс main__posts-container-img
-        postElement.classList.add("main__posts-container-img");
-
-        //заменяем содержимое созданного дива на пост
-        postElement.innerHTML = `
-<a href="${post.url}" target="_blank"><img src="${post.url}" alt="Картинка с котом ${post.id}" style="display:none;" /></a>
-<div class="main__posts-container-loader"></div>
-`;
-
-        //добавляем блок с текущим постом в конец дива со всеми постами
-        postsContainer.appendChild(postElement);
-      });
-    })
-
-    .catch((error) => {
-      console.error("Error: ", error);
-    });
+    if (response) {
+      //вызываем getImages и передаем ей массив json, полученный от сервера
+      getImages(response);
+    }
+  } catch (error) {
+    //обрабатываем ошибки ответа сервера
+    console.error(error.message);
+  } finally {
+    //скрываем лоадер при любом ответе сервера
+    hideLoader();
+  }
 };
 
-//3. Логика для кнопки Удалить посты
+//3. получаем картинки из пришедшего с сервера json
+const getImages = (arrImages) => {
+  if (arrImages) {
+    //проходим по массиву пришедших данных и для каждого элемента массива создаем див
+    arrImages.forEach((post) => {
+      //создаем див, куда будем выводить конкретный пост
+      const postElement = document.createElement("div");
+      //задаем созданному диву класс main__posts-container-img
+      postElement.classList.add("main__posts-container-img");
+
+      //выводим в див картинку + блок с лоадером
+      postElement.innerHTML = `
+<a href="${post.url}" target="_blank"><img src="${post.url}" alt="Картинка с котом ${post.id}" style="display:none;"/></a>
+<div class="main__posts-container-loader" style="display:block;"></div>
+`;
+      //добавляем блок с текущим постом в конец дива со всеми постами
+      postsContainer.appendChild(postElement);
+    });
+
+    //скрываем лоадер
+    hideLoader();
+  }
+};
+
+//4. скрыть лоадер
+function hideLoader() {
+  loader.style.display = "none";
+  img.style.display = "block";
+}
+
+//5. Логика для кнопки Удалить посты
 const cleanData = () => {
   postsContainer.innerHTML = "";
 };
 
-//4. Вешаем Обработчики Событий на кнопки
+//6. Вешаем Обработчики Событий на кнопки
 loadBtn.addEventListener("click", fetchData);
 clearBtn.addEventListener("click", cleanData);
-
-//если картинка загрузилась - прячем лоадер и показываем картинку
-img.addEventListener("load", function () {
-  loader.style.display = "none";
-  img.style.display = "block";
-});
-
-img.addEventListener('error', function() {
-  loader.style.display = 'none';
-  img.style.display = 'none';
-  console.error('Ошибка загрузки изображения');
-});
-/*
-
-не получается вставить данный код так, чтобы он менял стили у картинки и лоадера
-        img.addEventListener("load", function () {
-          loader.style.display = "none";
-          img.style.display = "block";
-        });
-*/
